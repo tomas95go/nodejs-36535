70a0ccacae7cbff9cb7fdbea459afddb14c8c476
tomas95go <tomas95go@gmail.com>
2022-06-07 14:10:26 -0300

    Instanciando la clase Usuario

--- Desafio_1/usuario.js
+++ Desafio_1/usuario.js
@@ -1,3 +1,4 @@
+//Alumno: González Oviedo, Tomás.
 class Usuario {
   constructor(nombre, apellido, libros, mascotas) {
     this.nombre = nombre;
@@ -5,4 +6,36 @@ class Usuario {
     this.libros = libros;
     this.mascotas = mascotas;
   }
+
+  getFullName() {
+    return `${this.apellido}, ${this.nombre}`;
+  }
+
+  addMascota(mascota) {
+    this.mascotas.push(mascota);
+  }
+
+  countMascotas() {
+    return this.mascotas.length;
+  }
+
+  addBook(nombre, autor) {
+    const libro = {
+      nombre,
+      autor,
+    };
+
+    this.libros.push(libro);
+  }
+
+  getBookNames() {
+    return this.libros.map(({ nombre }) => nombre);
+  }
 }
+
+const usuario = new Usuario(`Tomás`, `González Oviedo`, [], []);
+console.log(usuario.getFullName());
+usuario.addMascota(`Volt`);
+console.log(usuario.countMascotas());
+usuario.addBook(`Animal Farm`, `George Orwell`);
+console.log(usuario.getBookNames());
