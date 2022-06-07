57644e7f0c0f2be67ecaf30f4af52e563abb6999
tomas95go <tomas95go@gmail.com>
2022-06-07 12:54:10 -0300

    First Commit

---
+++ Desafio_1/usuario.js
@@ -0,0 +1,8 @@
+class Usuario {
+  constructor(nombre, apellido, libros, mascotas) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.libros = libros;
+    this.mascotas = mascotas;
+  }
+}
