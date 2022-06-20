f63cf62dbfed509738fba34e8578ea07ea87166e
tomas95go <tomas95go@gmail.com>
2022-06-20 02:33:54 -0300

    removiendo archivo sin uso

--- Desafio_5/routes/album.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const albumController = require("../controllers/album.controller");
-const albumRouter = express.Router();
-
-albumRouter.get(`/`, albumController.getNewAlbumForm);
-
-albumRouter.post(`/`, albumController.add);
-
-module.exports = albumRouter;
