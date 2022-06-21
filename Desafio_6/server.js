1c62de5d307930967f63ef1d2cd5a05a79d28d84
tomas95go <tomas95go@gmail.com>
2022-06-21 19:22:58 -0300

    Vistas basicas hechas con Handlebars

--- Desafio_6/server.js
+++ Desafio_6/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const { create } = require("express-handlebars");
+
 const app = express();
+const hbs = create({
+  partialsDir: ["views/partials/"],
+});
 const PORT = 8080;
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/views`);
+
 app.get("/", (req, res) => {
-  res.send(`Hola mundo!`);
+  res.render("partials/pages/home");
+});
+
+app.get("/productos", (req, res) => {
+  res.render("partials/pages/albums");
+});
+
+app.get("/chat", (req, res) => {
+  res.render("partials/pages/chat");
 });
 
 app.listen(PORT, () => {
