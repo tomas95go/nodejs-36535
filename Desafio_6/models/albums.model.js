7aa28d3e8ef2de5a41033081f4ac9d4ad3c65f25
tomas95go <tomas95go@gmail.com>
2022-06-23 19:53:35 -0300

    Terminada la parte de albums
    Se debe emprolijar el codigo

--- Desafio_6/models/albums.model.js
+++ Desafio_6/models/albums.model.js
@@ -1,18 +1,3 @@
-const albums = [
-  {
-    name: "AmeriKKKa's Most Wanted",
-    price: 29.99,
-    img: "https://i.discogs.com/ukKYqxz-LPzo4RXmvyOeRu4LtMzqtxzpCmwJ8chV1SQ/rs:fit/g:sm/q:90/h:500/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyOTE1/Ny0xMTk1MzgyNDYz/LmpwZWc.jpeg",
-    artist: "Ice Cube",
-    release_year: 1990,
-  },
-  {
-    name: "Get Rich Or Die Tryin'",
-    price: 29.99,
-    img: "https://i.discogs.com/AMVipM-vD1NU3NhvRZkzNkzCup5PakFAJJqVUj3UDXU/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI1ODg4/Mi0xNDI1NzA3OTQw/LTg4MDUuanBlZw.jpeg",
-    artist: "50 Cent",
-    release_year: 2003,
-  },
-];
+const albums = [];
 
 module.exports = albums;
